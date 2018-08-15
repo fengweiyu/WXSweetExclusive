b1a92c800c8ceebe6bc7f57a06d6bd3e192e548d
fengweiyu <984441750@qq.com>
2018-08-16 00:29:41 +0800

    成功实现背景与按钮跳动

--- pages/Express/Express.js
+++ pages/Express/Express.js
@@ -5,9 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    agreeStyle: "margin-top: 500px; margin-left: 20px;",
+    unagreeStyle: "margin-top: 500px; margin-left: 260px;",
   },
-
+  //事件处理函数
+  agree: function (e) {
+    if (e.type == "tap") {
+      console.log("enter love page");
+    }
+  },
+  unagree: function (e) {
+    if (e.type == "tap") 
+    {
+      var top = 500;
+      var left = 20+100;
+1
+      while ((left <= 120) && (top >= 455 && top <= 545))
+      {//同意按钮范围内的不允许
+        left=Math.random() * 260 + 1;  
+        top = Math.random() * 500 + 1;
+      }
+      console.log("margin-top: " + top + "px; margin-left: " + left + "px;");
+      this.setData({
+        unagreeStyle: "margin-top: " + top + "px; margin-left: "+left+"px;"
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
